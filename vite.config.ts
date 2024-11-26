5f8ca64ba9f6332383bff4d0d060903a216ed28b
FelipeDesigne <felipebdias98@gmail.com>
2024-11-26 18:15:15 -0300

    Simplificar configuração e corrigir carregamento de módulos

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/PixelArt/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    }
-  },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    minify: 'esbuild',
-    rollupOptions: {
-      input: resolve(__dirname, 'index.html'),
-      output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+    outDir: 'dist'
+  }
 });
