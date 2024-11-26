3b724e81cba5dc37451fb0bdffd162474e83bace
FelipeDesigne <felipebdias98@gmail.com>
2024-11-26 18:01:38 -0300

    Fix script paths and improve Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/PixelArt/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   build: {
     outDir: 'dist',
     sourcemap: false,
