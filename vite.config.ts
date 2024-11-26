9e684b84684f3b11ced58662126c946a2181c12e
FelipeDesigne <felipebdias98@gmail.com>
2024-11-26 17:53:05 -0300

    Update build for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,18 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/PixelArt/',
   plugins: [react()],
+  server: {
+    headers: {
+      'Content-Type': 'text/javascript',
+    },
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
   build: {
     outDir: 'dist',
-    sourcemap: true,
+    sourcemap: false,
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: undefined,
